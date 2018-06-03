634d625807b109010bc35263d025e5f6d8c3e75c
KKaramaligkas <konstantinos.karamaligkas@gmail.com>
2018-06-03 14:06:12 +0300

    Add project files.

---
+++ MovieRating/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MovieRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var title = "MovieTitle";
+            Random rate = new Random();// random generator
+            int rating = rate.Next(0,10); // rating from one person
+            int boxoffice = 8 * rate.Next(1000, 300000);// money earned from movie
+            Console.WriteLine(string.Format("{0} {1} {2}", title, rating, boxoffice));
+            string actorname = "Brad Pi";
+        }
+    }
+}
